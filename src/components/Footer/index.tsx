0fc86b2ffee184798d4e71dbe05cdca97ee09ce0
Kelman Dias dos Santos <69762912+k3lm4n@users.noreply.github.com>
2023-04-20 16:38:58 +0100

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="bg-white rounded-2xl">
+    <footer className="bg-white">
       <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <nav className="flex flex-wrap justify-center -mx-5 -my-2">
           <div className="px-5 py-2">
