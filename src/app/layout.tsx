a05617f2664cf116c452138edb661526d500def1
Kelman Dias dos Santos <69762912+k3lm4n@users.noreply.github.com>
2023-04-21 09:12:01 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,12 @@ export default function RootLayout({
   return (
     <QueryClientProvider client={client}>
       <html lang="en">
+        <head>
+          <title>SocialMate - A melhor comunidade de aprendizagem</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta charSet="utf-8" />
+          <link rel="shortcut icon" href="/unionLogo.svg" />
+        </head>
         <body className="w-full h-screen flex flex-row  bg-grey-200 rounded-r-xl">
           {children}
         </body>
